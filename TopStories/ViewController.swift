0aebcdb7bcedf12dc565abf367b9ebcf24c1b0bf
ChaosDragon789 <ethan.m.chang@gmail.com>
2019-02-20 11:14:44 -0600

    Initial Commit

---
+++ TopStories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopStories
+//
+//  Created by Ethan Chang on 2/20/19.
+//  Copyright © 2019 SomeAweApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
